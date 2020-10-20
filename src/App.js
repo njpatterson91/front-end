5ca6310f2c4410b92aae69be0559732af0cfb213
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2020-10-20 09:57:34 -0700

    single form

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Form from './components/SignupForms'
-import {v4 as uuid} from 'uuid'
-import * as yup from 'yup'
 import './App.css';
-import schema from './schema/schema'
-import styled from 'styled-components'
-
-
-/* const initialprofiles = [
-  {id: uuid(), name: 'Throckmorton', email:'sk8tercousin@hawtmail.com', password: 'coolsk8'},
-  {id: uuid(), name: 'Hue Janus', email:'HueJ@hautmail.com', password: 'hole'},
-  {id: uuid(), name: 'Phil Mckracken', email:'Philcrack@hautmail.com', password: 'Davey Jones Locker'},
-] */
 
 
 export default function App() {
-  
-  
-  /* const inputChange = (name, value) => {
-  yup
-    .reach(schema, name)
-    .validate(value) 
-    .then(() => {
-      setFormErrors({
-        ...formErrors,
-        [name]: "",
-      });
-    })
-    .catch((err) => {
-      setFormErrors({
-        ...formErrors,
-        [name]: err.errors[0],
-      });
-    });
-
-    setFormValues({
-      ...formValues,
-      [name]: value, 
-    });
-  }; */
-
-  
-
-  return Form
+  return <Form/>
 }
 
--- src/components/SignupForms.js
+++ src/components/SignupForms.js
@@ -1,160 +1,149 @@
-import {React, useState, useEffect} from 'react';
-import '../components/SignupForms';
-import styled from 'styled-components'
-import schema from '../schema/schema'
+import  React, {useState, useEffect } from "react";
+import "../components/SignupForms";
+import styled from "styled-components";
+import schema from "../schema/schema";
 
 const AppHead = styled.div`
-background-image: url('https://images.unsplash.com/photo-1575879911904-ca5d889c6c7e?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=4800');
+  background-image: url("https://images.unsplash.com/photo-1575879911904-ca5d889c6c7e?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=4800");
 
-background-size: 100%;
-`
+  background-size: 100%;
+`;
 const Title = styled.h2`
-margin-bottom: 0%auto;
-`
-const NiceDiv = styled.div `
-margin:3% 5% 5% 5%;
-font-weight: bold;
-`
+  margin-bottom: 0%auto;
+`;
+const NiceDiv = styled.div`
+  margin: 3% 5% 5% 5%;
+  font-weight: bold;
+`;
 const StyledInput = styled.input`
-border: solid 10px steelblue;
-border-radius: 20px;
-margin-top: 5%;
-&:hover {
-    color:blue;
+  border: solid 10px steelblue;
+  border-radius: 20px;
+  margin-top: 5%;
+  &:hover {
+    color: blue;
     transform: scale(1.1);
-     transition: all .5s ease-in-out;
-
-}
-transition: all .5s ease-in-out;
-
-`
+    transition: all 0.5s ease-in-out;
+  }
+  transition: all 0.5s ease-in-out;
+`;
 const StyledBtn = styled.button`
-font-size: 2rem;
-border-radius: 15px;
-border: solid skyblue ;
-background: steelblue;
-color: white;
-font-weight: bolder;
-margin-bottom: 5%;
-&:hover {
-    color:blue;
+  font-size: 2rem;
+  border-radius: 15px;
+  border: solid skyblue;
+  background: steelblue;
+  color: white;
+  font-weight: bolder;
+  margin-bottom: 5%;
+  &:hover {
+    color: blue;
     transform: scale(1.1);
-     transition: all .5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+  }
+  transition: all 0.5s ease-in-out;
+`;
+const initialprofiles = [];
 
-}
-transition: all .5s ease-in-out;
+const initialFormValues = {
+  name: "",
+  email: "",
+  password: "",
+};
+const initialFormErrors = {
+  username: "",
+  email: "",
+  password: "",
+  terms: "",
+};
+const initialDisabled = false;
 
-`
-    const initialprofiles= []
+export default function Form(props) {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+  const [profiles, setprofiles] = useState(initialprofiles);
 
-    const initialFormValues = {
-        name:'',
-        email: '',
-       password: '',
-      }
-      const initialFormErrors = {
-        username: "",
-        email: "",
-       password: "",
-        terms: "",
-      };
-      const initialDisabled = false;
+  const { values, submit, change, errors } = props;
 
-      
-      export default function Form(props){
-          const [formValues, setFormValues] = useState(initialFormValues)
-          const [formErrors, setFormErrors] = useState(initialFormErrors);
-          const [disabled, setDisabled] = useState(initialDisabled)
-          const [profiles, setprofiles] = useState(initialprofiles)
-          
-    const { values, submit, change, errors } = props;
- 
-           
-    const onSubmit = (evt) => {
-        evt.preventDefault();
-        submit();
-      };
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
 
-    const onChange = (evt) => {
-        const { name, value, type, checked } = evt.target;
-        const valueToUse = type === "checkbox" ? checked : value;
-        change(name, valueToUse);
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
+
+  const formSubmit = () => {
+    const newProfile = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
     };
+    setprofiles(...profiles, newProfile);
+  };
 
-    const formSubmit = () => {
-        const newProfile = {
-          name: formValues.name.trim(),
-          email: formValues.email.trim(),
-          password: formValues.password.trim(),
-        }
-        setprofiles(...profiles, newProfile)
-      }
-      
-    
-       useEffect(() => {
-        schema.isValid(formValues).then((valid) => {
-         setDisabled(!valid);
-        });
-      }, [formValues]);
-      console.log(profiles)
-    
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, []);
+
   return (
-            <form id ='theform' onSubmit = {onSubmit}>
-                <div className="App">
-                <header className="App-header">
-                    <Title>Potluck Planner</Title>
-                </header>
-                    <Form
-                     values={formValues}
-                     submit={formSubmit}
-                     disabled={disabled}
-                     errors={formErrors}
-                    />
-                 </div>
+       <div className='body'>
+  <div className="App">
+      <header className="App-header">
+          <Title>Potluck Planner</Title>
+      </header>
 
-        <AppHead className="App">
+  </div>
 
-            <div className="errors">
-                <div>{errors.name}</div>
-                <div>{errors.email}</div>
-                <div>{errors.password}</div>
-            </div>
+  <AppHead className="App">
+      <form id ='theform' onSubmit = {onSubmit}>
 
-            <NiceDiv>
-                <label htmlFor = 'name'> Name </label>
-                    <StyledInput 
-                    type = 'text'
-                    name = 'name'
-                    value = {values.name}
-                    onChange={onChange}
-                />
-            </NiceDiv>
+          <div className="errors">
+              <div>{formErrors.name}</div>
+              <div>{formErrors.email}</div>
+              <div>{formErrors.password}</div>
+          </div>
 
-            <NiceDiv>
-                <label htmlFor = 'email'>Email </label>
-                    <StyledInput 
-                        type = 'text'
-                        name = 'email'
-                        id = 'email'
-                        value = {values.email}
-                        onChange={onChange}
-                    />
-            </NiceDiv>
+          <NiceDiv>
+              <label htmlFor = 'name'> Name </label>
+                  <StyledInput 
+                  type = 'text'
+                  name = 'name'
+                  value = {formValues.name}
+                  onChange={onChange}
+              />
+          </NiceDiv>
 
-            <NiceDiv>
-            <label htmlFor = 'password'> Password</label>
-                <StyledInput 
-                    type ='text'
-                    id = 'password' 
-                    name = 'password'
-                    value = {values.password}
-                    onChange={onChange}
+          <NiceDiv>
+              <label htmlFor = 'email'>Email </label>
+                  <StyledInput 
+                      type = 'text'
+                      name = 'email'
+                      id = 'email'
+                      value = {formValues.email}
+                      onChange={onChange}
+                  />
+          </NiceDiv>
 
-                />
-            </NiceDiv>
+          <NiceDiv>
+              <label htmlFor = 'password'> Password</label>
+              <StyledInput 
+                  type ='text'
+                  id = 'password' 
+                  name = 'password'
+                  value = {formValues.password}
+                  onChange={onChange}
 
-            <StyledBtn id= 'signupBtn' type = 'signup' disabled = {disabled}>Sign Up!</StyledBtn>
-        </AppHead>
-    </form>
-  );
+              />
+          </NiceDiv>
+
+       <StyledBtn id= 'signupBtn' type = 'signup' disabled = {disabled}>Sign Up!</StyledBtn>
+      </form>
+  </AppHead>
+</div>
+  )
 }
