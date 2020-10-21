ad2c092692a5e05d0d7040b3ddebd9989393326c
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-10-21 13:47:18 -0400

    endpoint fix

--- src/components/homeMapDS.js
+++ src/components/homeMapDS.js
@@ -1,7 +1,7 @@
 import { axiosWithAuth } from "../utilities/axiosWithAuth"
 
 axiosWithAuth()
-    .get("https://potluck-planner-api.herokuapp.com/api/potlucks/odst0016/testPass")
+    .get("https://potluck-planner-api.herokuapp.com/api/potlucks")
     .then((res)=>{
         res.data.map((item)=>{
             return <div>{item.items}</div>
